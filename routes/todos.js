cc659e27db37cf1b694da93919d945c32d61d1c1
uday <uday@intelligrape.com>
2014-07-26 15:52:39 +0530

    Updated code for todo update

--- routes/todos.js
+++ routes/todos.js
@@ -1,38 +1,44 @@
 var express = require('express'),
-        router = express.Router(),
-        Todo = require('../domains/Todo.js');
+    router = express.Router(),
+    Todo = require('../domains/Todo.js');
 
 /* GET todos listing. */
 router.get('/', function (req, res) {
-    Todo.list(function(todos){
-        res.render('index', { title: 'Express',todos:todos,todo:{title:'',description:'',id:''}});
+    Todo.list(function (todos) {
+        res.render('index', { title: 'Express', todos: todos, todo: {title: '', description: '', id: ''}});
     })
 });
 
 router.get('/create', function (req, res) {
-    Todo.list(function(todos){
+    Todo.list(function (todos) {
         var id = req.param('id');
-        if(id){
-            Todo.find({id:id}).success(function(todo){
-                res.render('index', { title: 'Express',todos:todos,todo:todo});
+        if (id) {
+            Todo.find({id: id}).success(function (todo) {
+                res.render('index', { title: 'Express', todos: todos, todo: todo});
             })
-        }else{
-            res.render('index', { title: 'Express',todos:todos,todo:{title:'',description:'',id:''}});
+        } else {
+            res.render('index', { title: 'Express', todos: todos, todo: {title: '', description: '', id: ''}});
         }
 
     })
 });
 
 router.post('/save', function (req, res) {
-    Todo.create(req.body).success(function(){
-        res.redirect('/');
-    });
+    var id = req.param('id');
+    if (id) {
+        Todo.find(req.param('id')).on('success', function (todo) {
 
+        })
+    } else {
+        Todo.create(req.body).success(function () {
+            res.redirect('/');
+        });
+    }
 });
 
 router.get('/delete', function (req, res) {
-    Todo.find(req.param('id')).on('success', function(todo) {
-        todo.destroy().on('success', function(u) {
+    Todo.find(req.param('id')).on('success', function (todo) {
+        todo.destroy().on('success', function (u) {
             res.redirect('/');
         })
     })
